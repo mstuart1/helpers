f7a8163b68da1bd387575bf3c2da6de0d2921a5b
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-09-20 14:14:41 -0400

    working

--- scripts/map_anem.R
+++ scripts/map_anem.R
@@ -9,17 +9,23 @@
 
 map_anem <- function(){
   library(dplyr)
+  library(tidyr)
+  library(lubridate)
   source("scripts/con_leyte.R")
   source("scripts/date_dive.R")
 
+  begin <- paste(x, "01", "01", sep = "-")
+  end <- paste(x, "12", "31", sep = "-")
+  
   leyte <- conleyte()
   
   # pull dives for date range
-  dive <- date_dive("2016-01-01", "2016-12-30")
+  dive <- date_dive(begin, end)
   
   # reduce number of columns
   dive <- dive %>% 
-    select(dive_table_id, date, site)
+    select(dive_table_id, date, site, gps) %>% 
+    rename(unit = gps)
   
   # find anemones that match those dives and dates
   anem <- leyte %>%
@@ -28,339 +34,37 @@ map_anem <- function(){
     select(dive_table_id, anem_id, anem_obs, obs_time) %>%
     collect() %>% 
     separate(obs_time, into = c("hour", "minute", "second"), sep = ":") %>% 
-    mutate(gpx_hour = as.numeric(hour) - 8)
+    mutate(gpx_hour = as.numeric(hour) - 8) %>% 
+    mutate(minute = as.numeric(minute))
     
 
   anem <- left_join(anem, dive, by = "dive_table_id")
   rm(dive)
   
-  i <- min(anem$date)
-  j <- max(anem$date)
-
-    
-  latlong <- leyte %>%
+  # find the lat long for this anem
+  lat <- leyte %>%
     tbl("GPX") %>%
-    collect(n = Inf) %>% 
-    filter(time > i & time < j)
-    
+    mutate(date = date(time)) %>%
+    filter(date %in% anem$date) %>% 
+    mutate(gpx_hour = hour(time)) %>% 
+    mutate(minute = minute(time)) %>% 
+    mutate(second = second(time)) %>% 
+    select(-time, -second) %>% 
+    collect()
+  
+  sum_lat <- lat %>%
+    group_by(unit, date, gpx_hour, minute) %>% 
+    summarise(lat = mean(as.numeric(lat)),
+      lon = mean(as.numeric(lon)))
+  
+  anem <- left_join(anem, sum_lat, by = c("unit", "date", "gpx_hour", "minute"))
 
-anem$lat <- NA
-anem$lon <- NA
-for(i in 1:nrow(anem)){
-  #Get date and time information for the anemone
-  date <- as.character(anem$date[i])
-  datesplit <- strsplit(date,"-", fixed = T)[[1]]
-  year <- as.numeric(datesplit[1])
-  month <- as.numeric(datesplit[2])
-  day <- as.numeric(datesplit[3])
-  time <- as.character(anem$ObsTime[i])
-  timesplit <- strsplit(time, ":", fixed = T)[[1]]
-  hour <- as.numeric(timesplit[1])
-  min <- as.numeric(timesplit[2])
-  sec <- as.numeric(timesplit[3])
-  
-  # Convert time to GMT
-  hour <- hour - 8
-  if(!is.na(hour) & hour < 0){
-    day <- day - 1
-    hour <- hour + 24
-  }
-  
-  # Find the location records that match the date/time stamp (to nearest second)
-  latlongindex <- which(latlong$year == year & latlong$month == month & latlong$day == day & latlong$hour == hour & latlong$min == min)
-  i2 <- which.min(abs(latlong$sec[latlongindex] - sec))
-  
-  # Calculate the lat/long for this time
-  if(length(i2)>0){
-    anem$lat[i] = latlong$lat[latlongindex][i2]
-    anem$lon[i] = latlong$long[latlongindex][i2]
-  }
-}
-### WAIT ###
-anem$dive_table_id <- NULL
 
 # Write out for QGIS (has column headers)
-
-out <- anem[,c('lat', 'lon', 'date', "name", "anem_id", "anemobs")]
-
-# for samples that don't have an anemobs, replace with anem_id
-out$anemobs[is.na(anem$anemobs)] <- paste("#", out$anem_id[is.na(anem$anemobs)], sep = "")
-
-write.table(out, file=paste(Sys.Date(), "GPSSurvey_anemlatlong_", year, "_forQGIS.csv", sep = ""), col.names=TRUE, sep=',', row.names=FALSE, quote=TRUE)
-
-# 2015_05 anem data -------------------------------------------------------
-
-suppressWarnings (dive <- leyte %>% tbl("diveinfo") %>% filter(date > "2015-04-01" & date < "2016-01-01") %>% select(id, date, name) %>% collect())
-anem <- leyte %>% tbl("anemones") %>% filter(dive_table_id %in% dive$id & !is.na(anem_id)) %>% select(dive_table_id, anem_id, anemobs, ObsTime) %>% collect()
-anem <- left_join(anem, dive, by = c("dive_table_id" = "id"))
-rm(dive)
-latlong <- leyte %>% tbl("GPX") %>% filter(year == 2015) %>% collect()
-
-anem$lat <- NA
-anem$lon <- NA
-for(i in 1:nrow(anem)){
-  #Get date and time information for the anemone
-  date <- as.character(anem$date[i])
-  datesplit <- strsplit(date,"-", fixed = T)[[1]]
-  year <- as.numeric(datesplit[1])
-  month <- as.numeric(datesplit[2])
-  day <- as.numeric(datesplit[3])
-  time <- as.character(anem$ObsTime[i])
-  timesplit <- strsplit(time, ":", fixed = T)[[1]]
-  hour <- as.numeric(timesplit[1])
-  min <- as.numeric(timesplit[2])
-  sec <- as.numeric(timesplit[3])
-  
-  # Convert time to GMT
-  hour <- hour - 8
-  if(!is.na(hour) & hour < 0){
-    day <- day - 1
-    hour <- hour + 24
-  }
-  
-  # Find the location records that match the date/time stamp (to nearest second)
-  latlongindex <- which(latlong$year == year & latlong$month == month & latlong$day == day & latlong$hour == hour & latlong$min == min)
-  i2 <- which.min(abs(latlong$sec[latlongindex] - sec))
-  
-  # Calculate the lat/long for this time
-  if(length(i2)>0){
-    anem$lat[i] = latlong$lat[latlongindex][i2]
-    anem$lon[i] = latlong$long[latlongindex][i2]
-  }
-}
-### WAIT ###
-anem$dive_table_id <- NULL
-
-# Write out for QGIS (has column headers)
-
-out <- anem[,c('lat', 'lon', 'date', "name", "anem_id", "anemobs")]
-
-# for samples that don't have an anemobs, replace with anem_id
-out$anemobs[is.na(anem$anemobs)] <- paste("#", out$anem_id[is.na(anem$anemobs)], sep = "")
-
-write.table(out, file=paste(Sys.Date(), "GPSSurvey_anemlatlong_", year, "_05_forQGIS.csv", sep = ""), col.names=TRUE, sep=',', row.names=FALSE, quote=TRUE)
-
-# # 2015_01 anem data -------------------------------------------------------
+# out <- anem %>% 
+#   select(lat, lon, date, site, anem_id, anem_obs)
 # 
-# suppressWarnings (dive <- leyte %>% tbl("diveinfo") %>% filter(date > "2015-01-01" & date < "2015-04-01") %>% select(id, date, name) %>% collect())
-# anem <- leyte %>% tbl("anemones") %>% filter(dive_table_id %in% dive$id & !is.na(anem_id)) %>% select(dive_table_id, anem_id, anemobs, ObsTime) %>% collect()
-# anem <- left_join(anem, dive, by = c("dive_table_id" = "id"))
-# rm(dive)
-# latlong <- leyte %>% tbl("GPX") %>% filter(year == 2015) %>% collect()
-# 
-# anem$lat <- NA
-# anem$lon <- NA
-# 
-# for(i in 1:nrow(anem)){
-#   #Get date and time information for the anemone
-#   date <- as.character(anem$date[i])
-#   datesplit <- strsplit(date,"-", fixed = T)[[1]]
-#   year <- as.numeric(datesplit[1])
-#   month <- as.numeric(datesplit[2])
-#   day <- as.numeric(datesplit[3])
-#   time <- as.character(anem$ObsTime[i])
-#   timesplit <- strsplit(time, ":", fixed = T)[[1]]
-#   hour <- as.numeric(timesplit[1])
-#   min <- as.numeric(timesplit[2])
-#   sec <- as.numeric(timesplit[3])
-#   
-#   # Convert time to GMT
-#   hour <- hour - 8
-#   if(!is.na(hour) & hour < 0){
-#     day <- day - 1
-#     hour <- hour + 24
-#   }
-#   
-#   # Find the location records that match the date/time stamp (to nearest second)
-#   latlongindex <- which(latlong$year == year & latlong$month == month & latlong$day == day & latlong$hour == hour & latlong$min == min)
-#   i2 <- which.min(abs(latlong$sec[latlongindex] - sec))
-#   
-#   # Calculate the lat/long for this time
-#   if(length(i2)>0){
-#     anem$lat[i] = latlong$lat[latlongindex][i2]
-#     anem$lon[i] = latlong$long[latlongindex][i2]
-#   }
-# }
-# ### WAIT ###
-# anem$dive_table_id <- NULL
-# 
-# # Write out for QGIS (has column headers)
-# 
-# out <- anem[,c('lat', 'lon', 'date', "name", "anem_id", "anemobs")]
-# 
-# # for samples that don't have an anemobs, replace with anem_id
-# out$anemobs[is.na(anem$anemobs)] <- out$anem_id[is.na(anem$anemobs)]
-# 
-# write.table(out, file=paste(Sys.Date(), "GPSSurvey_anemlatlong_", year, "_01_forQGIS.csv", sep = ""), col.names=TRUE, sep=',', row.names=FALSE, quote=TRUE)
-# this data does not currently live on the database
-
-# 2014 anem data ----------------------------------------------------------
-
-suppressWarnings (dive <- leyte %>% tbl("diveinfo") %>% filter(date > "2014-01-01" & date < "2015-01-01") %>% select(id, date, name) %>% collect())
-anem <- leyte %>% tbl("anemones") %>% filter(dive_table_id %in% dive$id & !is.na(anem_id)) %>% select(dive_table_id, anem_id, anemobs, ObsTime) %>% collect()
-anem <- left_join(anem, dive, by = c("dive_table_id" = "id"))
-rm(dive)
-latlong <- leyte %>% tbl("GPX") %>% filter(year == 2014) %>% collect()
-
-anem$lat <- NA
-anem$lon <- NA
-for(i in 1:nrow(anem)){
-  #Get date and time information for the anemone
-  date <- as.character(anem$date[i])
-  datesplit <- strsplit(date,"-", fixed = T)[[1]]
-  year <- as.numeric(datesplit[1])
-  month <- as.numeric(datesplit[2])
-  day <- as.numeric(datesplit[3])
-  time <- as.character(anem$ObsTime[i])
-  timesplit <- strsplit(time, ":", fixed = T)[[1]]
-  hour <- as.numeric(timesplit[1])
-  min <- as.numeric(timesplit[2])
-  sec <- as.numeric(timesplit[3])
-  
-  # Convert time to GMT
-  hour <- hour - 8
-  if(!is.na(hour) & hour < 0){
-    day <- day - 1
-    hour <- hour + 24
-  }
-  
-  # Find the location records that match the date/time stamp (to nearest second)
-  latlongindex <- which(latlong$year == year & latlong$month == month & latlong$day == day & latlong$hour == hour & latlong$min == min)
-  i2 <- which.min(abs(latlong$sec[latlongindex] - sec))
-  
-  # Calculate the lat/long for this time
-  if(length(i2)>0){
-    anem$lat[i] = latlong$lat[latlongindex][i2]
-    anem$lon[i] = latlong$long[latlongindex][i2]
-  }
-}
-### WAIT ###
-anem$dive_table_id <- NULL
-
-# Write out for QGIS (has column headers)
-
-out <- anem[,c('lat', 'lon', 'date', "name", "anem_id", "anemobs")]
-
-# for samples that don't have an anemobs, replace with anem_id
-out$anemobs[is.na(anem$anemobs)] <- paste("#", out$anem_id[is.na(anem$anemobs)], sep = "")
-
-# remove lines without lat lon
-out <- out[!is.na(out$lat), ]
-
-write.table(out, file=paste(Sys.Date(), "GPSSurvey_anemlatlong_", year, "_forQGIS.csv", sep = ""), col.names=TRUE, sep=',', row.names=FALSE, quote=TRUE)
-
-# 2013 anem data ----------------------------------------------------------
-suppressWarnings (dive <- leyte %>% tbl("diveinfo") %>% filter(date > "2013-01-01" & date < "2014-01-01") %>% select(id, date, name) %>% collect())
-anem <- leyte %>% tbl("anemones") %>% filter(dive_table_id %in% dive$id & !is.na(anem_id)) %>% select(dive_table_id, anem_id, anemobs, ObsTime) %>% collect()
-anem <- left_join(anem, dive, by = c("dive_table_id" = "id"))
-rm(dive)
-latlong <- leyte %>% tbl("GPX") %>% filter(year == 2013) %>% collect()
-
-anem$lat <- NA
-anem$lon <- NA
-for(i in 1:nrow(anem)){
-  #Get date and time information for the anemone
-  date <- as.character(anem$date[i])
-  datesplit <- strsplit(date,"-", fixed = T)[[1]]
-  year <- as.numeric(datesplit[1])
-  month <- as.numeric(datesplit[2])
-  day <- as.numeric(datesplit[3])
-  time <- as.character(anem$ObsTime[i])
-  timesplit <- strsplit(time, ":", fixed = T)[[1]]
-  hour <- as.numeric(timesplit[1])
-  min <- as.numeric(timesplit[2])
-  sec <- as.numeric(timesplit[3])
-  
-  # Convert time to GMT
-  hour <- hour - 8
-  if(!is.na(hour) & hour < 0){
-    day <- day - 1
-    hour <- hour + 24
-  }
-  
-  # Find the location records that match the date/time stamp (to nearest second)
-  latlongindex <- which(latlong$year == year & latlong$month == month & latlong$day == day & latlong$hour == hour & latlong$min == min)
-  i2 <- which.min(abs(latlong$sec[latlongindex] - sec))
-  
-  # Calculate the lat/long for this time
-  if(length(i2)>0){
-    anem$lat[i] = latlong$lat[latlongindex][i2]
-    anem$lon[i] = latlong$long[latlongindex][i2]
-  }
-}
-### WAIT ###
-anem$dive_table_id <- NULL
-
-# Write out for QGIS (has column headers)
-
-out <- anem[,c('lat', 'lon', 'date', "name", "anem_id", "anemobs")]
-
-# for samples that don't have an anemobs, replace with anem_id
-out$anemobs[is.na(anem$anemobs)] <- paste("#", out$anem_id[is.na(anem$anemobs)], sep = "")
-
-# remove lines without lat lon
-out <- out[!is.na(out$lat), ]
-
-write.table(out, file=paste(Sys.Date(), "GPSSurvey_anemlatlong_", year, "_forQGIS.csv", sep = ""), col.names=TRUE, sep=',', row.names=FALSE, quote=TRUE)
+# name <- paste(Sys.Date(), "_GPSSurvey_anemlatlong_", x, "_forQGIS.csv", sep = "")
+# write.table(out, file = name, col.names=T, sep=',', row.names=F, quote=T)
 
 
-
-# 2012 anem data ----------------------------------------------------------
-
-
-suppressWarnings (dive <- leyte %>% tbl("diveinfo") %>% filter(date > "2012-01-01" & date < "2013-01-01") %>% select(id, date, name) %>% collect())
-anem <- leyte %>% tbl("anemones") %>% filter(dive_table_id %in% dive$id & !is.na(anem_id)) %>% select(dive_table_id, anem_id, anemobs, ObsTime) %>% collect()
-anem <- left_join(anem, dive, by = c("dive_table_id" = "id"))
-rm(dive)
-latlong <- leyte %>% tbl("GPX") %>% filter(year == 2012) %>% collect()
-
-anem$lat <- NA
-anem$lon <- NA
-for(i in 1:nrow(anem)){
-  #Get date and time information for the anemone
-  date <- as.character(anem$date[i])
-  datesplit <- strsplit(date,"-", fixed = T)[[1]]
-  year <- as.numeric(datesplit[1])
-  month <- as.numeric(datesplit[2])
-  day <- as.numeric(datesplit[3])
-  time <- as.character(anem$ObsTime[i])
-  timesplit <- strsplit(time, ":", fixed = T)[[1]]
-  hour <- as.numeric(timesplit[1])
-  min <- as.numeric(timesplit[2])
-  sec <- as.numeric(timesplit[3])
-  
-  # Convert time to GMT
-  hour <- hour - 8
-  if(!is.na(hour) & hour < 0){
-    day <- day - 1
-    hour <- hour + 24
-  }
-  
-  # Find the location records that match the date/time stamp (to nearest second)
-  latlongindex <- which(latlong$year == year & latlong$month == month & latlong$day == day & latlong$hour == hour & latlong$min == min)
-  i2 <- which.min(abs(latlong$sec[latlongindex] - sec))
-  
-  # Calculate the lat/long for this time
-  if(length(i2)>0){
-    anem$lat[i] = latlong$lat[latlongindex][i2]
-    anem$lon[i] = latlong$long[latlongindex][i2]
-  }
-}
-### WAIT ###
-anem$dive_table_id <- NULL
-
-# Write out for QGIS (has column headers)
-
-out <- anem[,c('lat', 'lon', 'date', "name", "anem_id", "anemobs")]
-
-# for samples that don't have an anemobs, replace with anem_id
-out$anemobs[is.na(anem$anemobs)] <- paste("#", out$anem_id[is.na(anem$anemobs)], sep = "")
-
-# remove lines without lat lon
-out <- out[!is.na(out$lat), ]
-
-write.table(out, file=paste(Sys.Date(), "GPSSurvey_anemlatlong_", year, "_forQGIS.csv", sep = ""), col.names=TRUE, sep=',', row.names=FALSE, quote=TRUE)
-
-
-
-}
