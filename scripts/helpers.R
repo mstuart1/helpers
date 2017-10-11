40d1045e8051e36ed61d4d9ca186dd0b8f528539
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-10-11 11:23:48 -0400

    add sampleid_date, anem_dive

--- scripts/helpers.R
+++ scripts/helpers.R
@@ -314,6 +314,78 @@ anem_date <- function(anem_tbl_id){
   return(day)
 }
 
+# sampleid_date ####
+#' which date was a sample_id captured?
+#' @export
+#' @name sampleid_date
+#' @author Michelle Stuart
+#' @param x = sample_id - the id of the fish for which you are trying to get a date
+#' @examples 
+#' date <- sampleid_date("APCL12_093")
+
+
+sampleid_date <- function(sampleid){
+  leyte <- read_db("Leyte")
+  
+  fish <- leyte %>% 
+    tbl("clownfish") %>% 
+    select(anem_table_id, sample_id) %>% 
+    collect() %>% 
+    filter(sample_id %in% sampleid)
+  
+  anem <- leyte %>% 
+    tbl("anemones") %>% 
+    select(dive_table_id, anem_table_id) %>% 
+    collect() %>% 
+    filter(anem_table_id %in% fish$anem_table_id) 
+  
+  day <- leyte %>% 
+    tbl("diveinfo") %>% 
+    select(date, dive_table_id) %>% 
+    collect() %>% 
+    filter(dive_table_id %in% anem$dive_table_id)
+  
+  day <- left_join(day, anem, by = "dive_table_id")
+  day <- left_join(day, fish, by = "anem_table_id") %>% 
+    select(sample_id, date)
+  
+  return(day)
+}
+
+# anem_dive ####
+#' for a given anemone , returns site, date, divetype
+#' @export
+#' @name anem_dive
+#' @author Michelle Stuart
+#' @param x = anem for which to get data
+#' @examples 
+#' info <- anem_dive(2183)
+
+anem_dive <- function(anemid){
+  leyte <- read_db("Leyte")
+  
+  # get the anem data  
+  anems <- leyte %>% 
+    tbl("anemones") %>% 
+    collect() %>% 
+    filter(anem_id %in% anemid)
+  
+  # retrieve date and site info for those anemones
+  dive <- leyte %>% 
+    tbl("diveinfo") %>%
+    collect() %>% 
+    filter(dive_table_id %in% anems$dive_table_id) %>% 
+    select(dive_table_id, site, date, dive_type)
+  
+  
+  # join dive info to anem info
+  anems <- left_join(anems, dive, by = "dive_table_id")
+  rm(dive)
+  return(anems)
+}
+
+
+
 
 
 
